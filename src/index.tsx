f28adfe32c15b8dd58a1dd802cee3328c9f0daff
BositG-7 <122206210+BositG-7@users.noreply.github.com>
2023-08-20 09:03:19 -0700

    edited

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import { MantineProvider } from "@mantine/core";
 import ReactDOM from "react-dom/client";
 import { Routes } from "routes";
 
+import Navbar from "components/navbar";
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
    <BrowserRouter>
       <MantineProvider>
          <h1>sakl</h1>
+         <Navbar />
          <Routes />
       </MantineProvider>
    </BrowserRouter>
