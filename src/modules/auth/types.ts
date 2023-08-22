ae3c22893b45662d4b35ad0dbd42be4f446e9a4f
NILUFAR <n1lufarmirathamova@gmail.com>
2023-08-22 21:40:17 +0500

    first commit

---
+++ src/modules/auth/types.ts
@@ -0,0 +1,42 @@
+import { GENDER, JOB } from "./constants";
+
+export namespace IEntity {
+   export interface User {
+      id: number;
+      firstName: string;
+      lastName: string;
+      phone: string;
+      image: string | null;
+      balance: string;
+      email: string;
+      username: string;
+      gender: GENDER;
+      job: JOB
+      birthday: string
+      about: string
+      is_active: boolean;
+      is_spiker: boolean;
+   }
+}
+export namespace IForm {
+   export interface Login {
+      username: string;
+      password: string;
+   }
+   export interface Register {
+      firstName?: string;
+      lastName?: string;
+      email: string;
+      username: string;
+      password: string;
+      confirmPassword: string;
+   }
+}
+
+export namespace IApi {
+   export namespace Register {
+      export interface Request extends IForm.Register {}
+      export interface Response extends IForm.Register {}
+   }
+}
+
