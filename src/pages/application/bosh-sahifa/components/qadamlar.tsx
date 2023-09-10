0c301855de8b95a8b6823c36c7ff321c65f465af
Fayzulloh <raxmatovfayzullo68@gmail.com>
2023-09-10 15:10:09 +0500

    Application edit

---
+++ src/pages/application/bosh-sahifa/components/qadamlar.tsx
@@ -0,0 +1,40 @@
+import video from "../../../../assets/images/video.png";
+
+import "../style/qadamlar.scss";
+
+function Steps() {
+   return (
+      <section className="qadamlar">
+         <div className="title">
+            <h3>Qadamlar</h3>
+            <h1>
+               Ta’lim berishni
+               <span> qanday</span> boshlash mumkin?
+            </h1>
+         </div>
+         <div className="boxes">
+            <div className="text">
+               <div className="box">
+                  <h3>Ro’yxatdan o’tish</h3>
+                  <p>
+                     Bilim ulashuvchi sifatida ro’yxatdan o’tish uchun <span>bu yerga</span> bosing
+                  </p>
+               </div>
+               <div className="box">
+                  <h3>Ma’lumotlarni to’ldirish</h3>
+                  <p>Platforma tomonidan so’ralgan barcha ma’lumotlar to’liq to’ldirilishi zarur</p>
+               </div>
+               <div className="box">
+                  <h3>Kurs yuklash</h3>
+                  <p>Sohangiz bo’yicha kurs yuklashni boshlash</p>
+               </div>
+            </div>
+            <div className="image">
+               <img src={video} alt="video" />
+            </div>
+         </div>
+      </section>
+   );
+}
+
+export default Steps;
--- src/pages/application/bosh-sahifa/components/yangi-kurslar.tsx
+++ src/pages/application/bosh-sahifa/components/yangi-kurslar.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
-import { CourseTop } from "modules/auth/api";
+import { useEffect,useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { CourseNew } from "modules/auth/api";
 
-import "../style/top-course.scss";
-import "react-multi-carousel/lib/styles.css";
+import "../style/yangi-kurslar.scss";
 
 export interface Results {
    id: number;
@@ -13,19 +13,21 @@ export interface Results {
    review_count: number;
 }
 
-export interface TopCoursess {
+export interface NewCourses {
    count: number;
    next: null;
    previous: null;
    results: Results[];
 }
 
-function TopCourse({ deviceType }: any) {
-   const [pricing, setPricing] = useState<TopCoursess[]>();
+function NewCourse() {
+   const [pricing, setPricing] = useState<NewCourses[]>();
+
+   const navigete = useNavigate()
 
    useEffect(() => {
       const fetch = async () => {
-         const { data }: any = await CourseTop();
+         const { data }: any = await CourseNew();
 
          console.log(data.results);
          setPricing(data.results);
@@ -39,7 +41,7 @@ function TopCourse({ deviceType }: any) {
       <section className="top-course">
          <div className="title">
             <h1>
-               <span>Top</span> kurslar
+               <span>Yangi</span> kurslar
             </h1>
          </div>
          <div className="boxes">
@@ -67,16 +69,28 @@ function TopCourse({ deviceType }: any) {
                         </div>
                      </div>
                      <div className="price">
-                        {/* @ts-ignore */}
-                        <h3>{item.price}<span>/so'm</span></h3>
+                        <h3>{/* @ts-ignore */}
+                           {item.price}
+                           <span>/so'm</span>
+                        </h3>
                         <h2>Xarid qilish</h2>
                      </div>
                   </div>
                </div>
             ))}
          </div>
+         <div className="link">
+         <button
+               className="share-subject"
+               onClick={() => {
+                  navigete("/kurslar");
+               }}
+            >
+               Barchasi
+            </button>
+         </div>
       </section>
    );
 }
 
-export default TopCourse;
+export default NewCourse;
